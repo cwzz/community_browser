55bc28849249c9329f8ab1653da6f07ebdb98ee2
161250005 <1046277348@qq.com>
2019-05-30 21:50:33 +0800

    发帖页面完成(表单提交有问题)

---
+++ src/components/wangeditor/wangeditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="editor">
+    <div ref="toolbar" class="toolbar"></div>
+    <div ref="editor" class="text"></div>
+  </div>
+</template>
+
+<script>
+  import E from 'wangeditor'
+
+  export default {
+    name: 'Editorbar',
+    data () {
+      return {
+        editor:null,
+        info_:null,
+        editorContent: ''
+      }
+    },
+    model:{
+      prop:'value',
+      event:'change'
+    },
+    methods: {
+      getContent: function () {
+        alert(this.editorContent)
+      }
+    },
+    mounted() {
+      var editor = new E(this.$refs.editor)
+      editor.customConfig.onchange = (html) => {
+        this.editorContent = html
+      }
+      editor.create()
+    }
+  }
+</script>
+
+<style scoped>
+</style>
