7553c71388fab7826375e1bdc4b66e72347e08fa
jane <779761563@qq.com>
2019-06-04 17:31:51 +0800

    jane upload

---
+++ src/pages/UserMessagePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="background-color: #FFFFFF;padding: 10px">
+    <Menu ref="head"></Menu>
+    <Layout>
+      <Layout>
+        <Sider>
+          <Main ref="test"></Main>
+        </Sider>
+        <Layout>
+          <Content :style="{padding: '80px', minHeight: '280px', background: '#fff'}">
+            信息通知
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  </div>
+</template>
+
+<script>
+  import Menu from '../components/Menu/Menu'
+  import Main from '../pages/UserPage'
+    export default {
+        name: "UserMessagePage",
+      components:{Main,Menu},
+      mounted(){
+        this.$refs.head.active_index=4;
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,12 @@ import DetailPage from '@/pages/DetailPage'
 import HomePage from '@/pages/HomePage'
 import PublishPage from '@/pages/PublishPage'
 import UserPage from '@/pages/UserPage'
+import UserPublishPage from '@/pages/UserPublishPage'
+import UserCollectPage from '@/pages/UserCollectPage'
+import UserFollowPage from '@/pages/UserFollowPage'
+import UserFansPage from '@/pages/UserFansPage'
+import UserInfoPage from '@/pages/UserInfoPage'
+import UserMessagePage from "@/pages/UserMessagePage";
 
 Vue.use(Router)
 
@@ -36,5 +42,35 @@ export default new Router({
       name: 'UserPage',
       component: UserPage
     },
+    {
+      path: '/user/publish',
+      name: 'UserPublishPage',
+      component:UserPublishPage
+    },
+    {
+      path:'/user/collect',
+      name:'UserCollectPage',
+      component:UserCollectPage
+    },
+    {
+      path:'/user/follow',
+      name:'UserFollowPage',
+      component:UserFollowPage
+    },
+    {
+      path:'/user/fans',
+      name:'UserFansPage',
+      component:UserFansPage
+    },
+    {
+      path: '/user/info',
+      name:'UserInfoPage',
+      component:UserInfoPage
+    },
+    {
+      path:'/user/message',
+      name:'UserMessagePage',
+      component:UserMessagePage
+    }
   ]
 })
